fcf8eae2d03ccb94e5b8d9b8b3574ec97d3ed012
Eduardo Costa <m4c0@users.noreply.github.com>
2026-04-19 20:46:59 +0100

    standalone: window + apple menu

--- standalone.m
+++ standalone.m
@@ -1,18 +1,43 @@
 #import <AppKit/AppKit.h>
-#import <CoreFoundation/CoreFoundation.h>
 
-// Not really needed until you want to listen for NSApplication events
-@interface App : NSApplication
+@interface POCAppDelegate : NSObject<NSApplicationDelegate>
 @end
-@implementation App
+@implementation POCAppDelegate
+// Important if you use NSWindowStyleMaskClosable
+- (BOOL)applicationShouldTerminateAfterLastWindowClosed:(NSApplication *)app {
+  return YES;
+}
 @end
 
-int main() {
+static void run() {
   NSWindow * w = [NSWindow new];
+  w.styleMask = NSWindowStyleMaskTitled | NSWindowStyleMaskClosable | NSWindowStyleMaskMiniaturizable | NSWindowStyleMaskResizable;
+
+  NSRect crect = NSMakeRect(0, 0, 300, 200);
+  NSRect frect = [w frameRectForContentRect:crect];
+  [w setFrame:frect display:YES];
+  [w center];
   [w makeKeyAndOrderFront:w];
 
-  // You have to call using the custom class
-  NSApplication * a = [App sharedApplication];
+  // Apple menu
+  NSMenu * bar = [NSMenu new];
+  NSMenuItem * app_item = [NSMenuItem new];
+  NSMenu * app_menu = [NSMenu new];
+  [app_menu addItem:[[NSMenuItem alloc] initWithTitle:@"Quit le app"
+                                               action:@selector(terminate:)
+                                        keyEquivalent:@"q"]];
+  app_item.submenu = app_menu;
+  [bar addItem:app_item];
+
+  NSApplication * a = [NSApplication sharedApplication];
+  a.delegate = [POCAppDelegate new];
+  a.mainMenu = bar;
   [a activateIgnoringOtherApps:YES];
   [a run];
+}
+
+int main() {
+  @autoreleasepool {
+    run();
+  }
 }
