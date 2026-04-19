dae269e3046238f8b77a82416eeee142a718a56a
Eduardo Costa <m4c0@users.noreply.github.com>
2026-04-19 20:18:06 +0100

    build: tweaks

--- build.c
+++ build.c
@@ -27,39 +27,25 @@ static int run(char ** args) {
   return 1;
 }
 
-static int compile(char * name) {
-  char src[1024];
-  sprintf(src, "%s.c", name);
-
+static int cc(char * src, char * exe) {
   char * args[] = {
     "clang", "-Wall", "-g",
     "-framework", "AppKit",
-    "-o", name, src, 0 };
-  if (run(args)) return 1;
-
-  return 0;
+    "-o", exe, src, 0 };
+  return run(args);
 }
-
-static int appinfo_example() {
-  { char * args[] = {
-      "clang", "-Wall", "-g",
-      "-framework", "AppKit",
-      "-o", "app-info", "app-info.m", 0 };
-    if (run(args)) return 1; }
-
-  { char * args[] = { "cp", "app-info", "app-info.app/Contents/MacOS/", 0 };
-    if (run(args)) return 1; }
-
-  return 0;
-}
+#define compile(S) cc(S ".c", S)
 
 int main(int argc, char ** argv) {
   if (argc != 1) return (usage(), 1);
 
-  if (compile("build"       )) return 1;
   if (compile("objc-runtime")) return 1;
 
-  if (appinfo_example()) return 1;
+  // Use this example to see the different behaviour between running inside or
+  // outside a bundle. Mainly, notice how Cmd-Q propagates to terminal when you
+  // run outside the bundle
+  if (cc("app-info.m", "app-info")) return 1;
+  if (cc("app-info.m", "app-info.app/Contents/MacOS/app-info")) return 1;
 
   return 0;
 }
